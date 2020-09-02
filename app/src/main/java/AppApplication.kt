c9a40c07e74c0a6fb68c553799d10256d0634f4a
liang.gao <surcreak@163.com>
2020-09-02 20:39:09 +0800

    feat($view): add view.

--- app/src/main/java/AppApplication.kt
+++ app/src/main/java/AppApplication.kt
@@ -3,7 +3,9 @@ package com.surcreak.packidea
 import android.content.Context
 import androidx.multidex.MultiDex
 import com.surcreak.packidea.base.application.BaseApplication
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class AppApplication : BaseApplication() {
 
     override fun onCreate() {
--- libs/base/src/main/java/com/surcreak/packidea/base/ui/activity/BaseActivity.kt
+++ libs/base/src/main/java/com/surcreak/packidea/base/ui/activity/BaseActivity.kt
@@ -2,6 +2,8 @@ package com.surcreak.packidea.base.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.HiltAndroidApp
+
 
 abstract class BaseActivity: AppCompatActivity () {
     abstract fun getLayoutId(): Int
